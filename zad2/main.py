835bc3db772c49c277b48d5731d972e57102d538
shreq <shrek.kresh@o2.pl>
2020-04-03 18:16:07 +0200

    added matrix reducement

--- zad2/main.py
+++ zad2/main.py
@@ -8,7 +8,32 @@ def saddle_point(matrix):
     return min_for_row.max() == max_for_col.min()
 
 
+def is_dominant(master, slave):
+    if len(master) != len(slave):
+        raise Exception("Arrays' lengths do not match")
+
+    for i in range(len(master)):
+        if master[i] <= slave[i]:
+            return False
+    return True
+
+
+def reduce_dominated(matrix):
+    for master_label, master_content in matrix.iterrows():
+        for slave_label, slave_content in matrix.iterrows():
+            if master_label != slave_label and is_dominant(master_content, slave_content):
+                matrix.drop(labels=slave_label, inplace=True)
+    for master_label, master_content in matrix.iteritems():
+        for slave_label, slave_content in matrix.iteritems():
+            if master_label != slave_label and is_dominant(master_content, slave_content):
+                matrix.drop(columns=slave_label, inplace=True)
+
+
 rules = pandas.read_csv('rules.csv')
 rules.index = rules.columns.values
 
+print("There is " + ("" if saddle_point(rules) else "no ") + "saddle point")
+print(rules)
+print()
+reduce_dominated(rules)
 print(rules)
