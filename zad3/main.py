56e025781f8eaa650bb774bcc3a4a01063f53e21
pawci0 <pgalewicz@gmail.com>
2020-05-09 17:35:42 +0200

    fixed issues with task 3

--- zad3/main.py
+++ zad3/main.py
@@ -1,21 +1,28 @@
 import pandas
 
-
-def get_cheapest_connection(node, df):
-    nodes = df.iloc[node - 1, :]
-    return nodes.loc[nodes.eq(nodes.min())]
-
-
-data = pandas.read_csv('graph.csv').dropna(axis=1, how='all').dropna(how='all')
+data = pandas.read_csv('graph.csv')
 data.index += 1
 
 print(data.to_string())
+rows = data.index.values
+columns = data.columns.values
+columns = columns[columns != str(rows[0])]
+vertices = [rows[0]]
+
 
 result = pandas.DataFrame()
-for i in range(1, data.index.size + 1):
-    edges = get_cheapest_connection(i, data)
-    for j in range(edges.index.size):
-        result[str(i) + '-' + edges.index[j]] = [edges[j]]
+while len(columns) > 0:
+    nodes = data.loc[vertices, columns]
+    min_value = nodes.min().min()
+
+    vertex = nodes[nodes.eq(min_value).any(1)].index.values[0]
+    nearest_vertex = nodes.min().idxmin()
+
+    connection = str(vertex) + "-" + str(nearest_vertex)
+    result[connection] = [min_value]
+
+    vertices.append(int(nearest_vertex))
+    columns = columns[columns != nearest_vertex]
 
 print('\nEdges:\n' +
       result.T.to_string(header=False) +
